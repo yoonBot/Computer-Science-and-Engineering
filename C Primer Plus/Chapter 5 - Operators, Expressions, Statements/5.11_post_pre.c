0566190e3fa99db11c3cb036d59c34dd688a708e
yoonBot <yoon.ohbot@gmail.com>
2021-01-04 14:19:08 +0900

    Add files via upload

---
+++ C Primer Plus/Chapter 5 - Operators, Expressions, Statements/5.11_post_pre.c
@@ -0,0 +1,17 @@
+/*  post_pre.c -- postfix vs prefix  */
+
+#include <stdio.h>
+
+int main(void)
+{
+    int a = 1, b = 1;
+    int aplus, plusb;
+    aplus = a++;            /*  postfix  */
+    plusb = ++b;            /*  prefix   */
+    printf("a   aplus   b   plusb \n");
+    printf("%ld %5d %5d %5d\n", a, aplus, b, plusb);
+    
+    return 0;
+}
+
+           
